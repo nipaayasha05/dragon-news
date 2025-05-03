c92b0643953d4a58da734062f7cd681b656e7e33
nipaayasha05 <nipaayasha05@gmail.com>
2025-05-03 23:52:50 +0600

    second commit

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <span className="loading loading-spinner text-info"></span>
+      <span className="loading loading-spinner text-success"></span>
+      <span className="loading loading-spinner text-warning"></span>
+      <span className="loading loading-spinner text-info"></span>
+      <span className="loading loading-spinner text-success"></span>
+      <span className="loading loading-spinner text-warning"></span>
+      <span className="loading loading-spinner text-info"></span>
+      <span className="loading loading-spinner text-success"></span>
+      <span className="loading loading-spinner text-warning"></span>
+    </div>
+  );
+};
+
+export default Loading;
